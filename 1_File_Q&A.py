cd626e8861528870990a606570de73edc50c15f4
PPRAD914 <32915354+PPRAD914@users.noreply.github.com>
2024-01-25 18:47:39 -0600

    Update 1_File_Q&A.py

--- 1_File_Q&A.py
+++ 1_File_Q&A.py
@@ -14,20 +14,11 @@ question = st.text_input(
     disabled=not uploaded_file,
 )
 
-if uploaded_file and question and not anthropic_api_key:
-    st.info("Please add your Anthropic API key to continue.")
+col1, col2 = st.columns(2)
 
-if uploaded_file and question and anthropic_api_key:
-    article = uploaded_file.read().decode()
-    prompt = f"""{anthropic.HUMAN_PROMPT} Here's an article:\n\n<article>
-    {article}\n\n</article>\n\n{question}{anthropic.AI_PROMPT}"""
+original = Image.open(image)
+col1.header("Original")
+col1.image(original, use_column_width=True)
 
-    client = anthropic.Client(api_key=anthropic_api_key)
-    response = client.completions.create(
-        prompt=prompt,
-        stop_sequences=[anthropic.HUMAN_PROMPT],
-        model="claude-v1",  # "claude-2" for Claude 2 model
-        max_tokens_to_sample=100,
-    )
     st.write("### Answer")
     st.write(response.completion)
