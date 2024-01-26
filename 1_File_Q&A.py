9b5669f832994f82138cea4bcec86476e426a387
PPRAD914 <32915354+PPRAD914@users.noreply.github.com>
2024-01-25 18:53:25 -0600

    Update 1_File_Q&A.py

--- 1_File_Q&A.py
+++ 1_File_Q&A.py
@@ -16,7 +16,8 @@ question = st.text_input(
 col1, col2 = st.columns(2)
 
 #original = Image.open(image)
-col1.header("Original")
+col1.header("Anthropic")
+col1.header("Cohere")
 #col1.image(original, use_column_width=True)
 
 st.write("### Answer")
