0a91d9b0b8cbc8530ec33c780cde67950b8bf9fe
PPRAD914 <32915354+PPRAD914@users.noreply.github.com>
2024-01-25 18:44:08 -0600

    Update 1_File_Q&A.py

--- 1_File_Q&A.py
+++ 1_File_Q&A.py
@@ -1,8 +1,11 @@
 import streamlit as st
 import anthropic
+import cohere
 
 with st.sidebar:
     anthropic_api_key = st.text_input("Anthropic API Key", key="file_qa_api_key", type="password")
+    cohere_api_key = st.text_input("Cohere API Key", key="cohere_api_key", type="password")
+
     "[View the source code](https://github.com/streamlit/llm-examples/blob/main/pages/1_File_Q%26A.py)"
     "[![Open in GitHub Codespaces](https://github.com/codespaces/badge.svg)](https://codespaces.new/streamlit/llm-examples?quickstart=1)"
 
