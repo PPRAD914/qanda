5d504946e5d083bce822a4155e2de753104d9e1a
PPRAD914 <32915354+PPRAD914@users.noreply.github.com>
2024-01-25 18:56:51 -0600

    Update 1_File_Q&A.py

--- 1_File_Q&A.py
+++ 1_File_Q&A.py
@@ -5,7 +5,7 @@ with st.sidebar:
     "[View the source code](https://github.com/streamlit/llm-examples/blob/main/pages/1_File_Q%26A.py)"
     "[![Open in GitHub Codespaces](https://github.com/codespaces/badge.svg)](https://codespaces.new/streamlit/llm-examples?quickstart=1)"
 
-st.title("📝 File Q&A with Anthropic")
+st.title("📝 File Q&A with OpenAI and Cohere")
 uploaded_file = st.file_uploader("Upload an article", type=("txt", "md"))
 question = st.text_input(
     "Ask something about the article",
@@ -16,9 +16,9 @@ question = st.text_input(
 col1, col2 = st.columns(2)
 
 #original = Image.open(image)
-col1.header("Anthropic")
-col1.write("### Anthropic Answer")
-col2.header("Cohere")
+col1.header("OpenAI GPT-3.5")
+col1.write("#### Anthropic Answer")
+col2.header("Cohere Command 52B")
 col2.write("### Cohere Answer")
 #col1.image(original, use_column_width=True)
 
